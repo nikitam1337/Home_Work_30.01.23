450dcf79c570e2a7f4855df7b44de78559947f22
Nikita_Lyskov <nikitam.l@mail.ru>
2023-02-04 17:39:17 +0300

    Stap 1 is comleated

--- tg_calc_main.py
+++ tg_calc_main.py
@@ -1,14 +1,11 @@
 import telebot
 from telebot import types
-import datetime
-import random
 
 bot = telebot.TeleBot("5839553872:AAFi3Z5o1w-LMX3nJzUug3P-5-1X2xgaSwY")
 
 a = 0
 b = 0
 znak = ""
-
 
 @bot.message_handler(commands=["start"])
 def start(message):
@@ -34,11 +31,10 @@ def controller(message):
         bot.send_message(message.chat.id, "Введите два числа через пробел")
         bot.register_next_step_handler(message, user_numbers)
     elif message.text == "Комплексные числа":
-        bot.send_message(
-            message.chat.id, f"текущее время - {datetime.datetime.now()}")
+        bot.send_message(message.chat.id, "Введите два числа через пробел")
+        bot.register_next_step_handler(message, user_complex_numbers)
     else:
-        bot.send_message(message.chat.id, "Ошибка")
-
+        bot.send_message(message.chat.id, "Ошибка, введите команду: /start")
 
 def user_numbers(message):
     global a,b
@@ -46,6 +42,13 @@ def user_numbers(message):
     a = int(numbers.split()[0])
     b = int(numbers.split()[1])
     button_znaki_int(message)
+
+def user_complex_numbers(message):
+    global a,b
+    numbers = message.text
+    a = complex(numbers.split()[0])
+    b = complex(numbers.split()[1])
+    button_znaki_complex(message)
 
 def button_znaki_int(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -64,6 +67,19 @@ def button_znaki_int(message):
     bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
     bot.register_next_step_handler(message, operators)
 
+def button_znaki_complex(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    but1 = types.KeyboardButton("+")
+    but2 = types.KeyboardButton("-")
+    but3 = types.KeyboardButton("*")
+    but4 = types.KeyboardButton("/")
+    markup.add(but1)
+    markup.add(but2)
+    markup.add(but3)
+    markup.add(but4)
+    bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
+    bot.register_next_step_handler(message, operators)
+
 @bot.message_handler(content_types="text")
 def operators(message):
     global a,b
@@ -71,7 +87,22 @@ def operators(message):
     if message.text == "+":
         result = a + b
         bot.send_message(message.chat.id, f'{a} + {b} = {result}')
-
-
+    elif message.text == "-":
+        result = a - b
+        bot.send_message(message.chat.id, f'{a} - {b} = {result}')
+    elif message.text == "*":
+        result = a * b
+        bot.send_message(message.chat.id, f'{a} * {b} = {result}')
+    elif message.text == "/":
+        result = a / b
+        bot.send_message(message.chat.id, f'{a} / {b} = {result}')
+    elif message.text == "%":
+            result = a % b
+            bot.send_message(message.chat.id, f'{a} % {b} = {result}')
+    elif message.text == "//":
+        result = a // b
+        bot.send_message(message.chat.id, f'{a} // {b} = {result}')
+    else:
+        bot.send_message(message.chat.id, "Ошибка, введите команду: /start")
 
 bot.infinity_polling()
